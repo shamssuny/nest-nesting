886b693df5e30d3ee36f33891f349fa6d033fec8
shams@ticonsys.com <shams@ticonsys.com>
2026-06-16 13:17:51 +0600

    restart

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoffeeModule } from './coffee/coffee.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    CoffeeModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'pass123',
-      database: 'postgres',
-      //entities: [],
-      autoLoadEntities: true,
-      synchronize: true,
-    })
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/coffee/dto/update-coffee.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateCoffeeDto } from "./create-coffee.dto";
-
-export class updateCoffeeDto extends PartialType(CreateCoffeeDto){
-    
-}
